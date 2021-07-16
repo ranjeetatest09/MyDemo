41dd78d0acc3a8ea40df9046f882d9e7cc0a6147
Ranjeeta Rajput <ranjeeta.rajput@cerence.com>
2021-07-16 12:52:25 +0530

    test1

--- Sources/MYSPMDemo/MYSPMDemo.swift
+++ Sources/MYSPMDemo/MYSPMDemo.swift
@@ -1,6 +1,10 @@
 public struct MYSPMDemo {
     var text = "Hello, World!"
     static public func sum(a:Int, b: Int) -> Int {
+        return MYSPMDemo.result(a: a, b: b)
+    }
+    
+    static private func result(a: Int, b: Int) -> Int {
         return a + b
     }
 }
